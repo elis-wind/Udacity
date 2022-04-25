5e81d0d7e7e6d5ea13463003ffc1158ffefd14fd
elis-wind <nbobkova174@gmail.com>
2022-04-25 17:13:57 +0200

    enron outliers ML

--- Machine-Learning-Engineer/g-Outliers/enron_outliers.py
+++ Machine-Learning-Engineer/g-Outliers/enron_outliers.py
@@ -9,9 +9,20 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb") )
+# to remove the outlier which is a spreadsheet quirk
+data_dict.pop("TOTAL", 0 )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+#matplotlib.pyplot.savefig('enron_outliers.png')
+matplotlib.pyplot.savefig('enron_without_outliers.png')
+matplotlib.pyplot.show()
