8161c4c31481dbc22827ab270c88543e637c6d8d
elis-wind <nbobkova174@gmail.com>
2022-05-17 23:50:11 +0200

    author id with linear kernel ML SVM

---
+++ Machine-Learning-Engineer/SVM/author_id.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+#########################################################
+### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(gamma='auto',kernel='linear')
+
+clf.fit(features_train, labels_train)
+predictions = clf.predict(features_test)
+
+print(accuracy_score(labels_test, predictions))
+
+#########################################################
+
+#########################################################
+'''
+You'll be Provided similar code in the Quiz
+But the Code provided in Quiz has an Indexing issue
+The Code Below solves that issue, So use this one
+'''
+
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
+
+#########################################################
