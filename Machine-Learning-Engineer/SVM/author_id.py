59897248f509aa863c23b4fcaf0a42993cedb9f1
elis-wind <nbobkova174@gmail.com>
2022-05-18 00:13:52 +0200

    author id with hp tuning ML SVM

--- Machine-Learning-Engineer/SVM/author_id.py
+++ Machine-Learning-Engineer/SVM/author_id.py
@@ -21,15 +21,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-clf = SVC(gamma='auto',kernel='linear')
+clf = SVC(gamma='auto', kernel='rbf', C=10000)
+
+### TO SPEED UP TIME and optimize SVC parameters
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
 
 clf.fit(features_train, labels_train)
 predictions = clf.predict(features_test)
 
 print(accuracy_score(labels_test, predictions))
+
+###Printing the predicted label for 10th, 26th and 50th example
+#print(predictions[10])
+#print(predictions[26])
+#print(predictions[50])
+
+###Printing the numer of predictions for Chris and Sarah
+#print("Chris:", np.sum(predictions))
+#print("Sarah:", len(predictions) - np.sum(predictions))
 
 #########################################################
 
