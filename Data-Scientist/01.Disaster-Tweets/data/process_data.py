d93509f82c7fce3e77e0f42b06ccb9168bbeb063
elis-wind <nbobkova174@gmail.com>
2022-08-17 14:05:39 +0200

    add ETL py pipeline DS Disaster tweets project

--- Data-Scientist/01.Disaster-Tweets/data/process_data.py
+++ Data-Scientist/01.Disaster-Tweets/data/process_data.py
@@ -1,19 +1,84 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load data
+
+    Arguments:
+        messages_filepath(DataFrame): path of data frame with messages
+        categories_filepath(DataFrame): path of data frame with categories
+
+    Outputs:
+        df(DataFrame): data frame with concatenated messages and categories
+    """
+    # load messages and categories datasets
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = messages.merge(categories, how='inner', on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Clean data
+
+    Arguments:
+        df(DataFrame): raw data frame with concatenated messages and categories
+
+    Outputs:
+        df(DataFrame): clean data frame with concatenated messages and categories
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(";", expand=True)
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+    # use this row to extract a list of new column names for categories
+    category_colnames = row.apply(lambda x: x[:-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # drop duplicates
+    df.drop_duplicates(keep="first", inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save data into Data Base
+
+    Arguments:
+        df(DataFrame): clean data frame with concatenated messages and categories
+        database_filename(str): path of database file to output
+    """    
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('DisasterData', engine, index=False)  
 
 
 def main():
+    """
+    Main function for data Processing implements the ETL pipeline:
+        1) Data extraction from .csv
+        2) Data cleaning and pre-processing
+        3) Data loading to SQLite database
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
