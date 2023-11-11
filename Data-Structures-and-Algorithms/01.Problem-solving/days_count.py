601dbf35447f343c9d3b2e80a6288e89792b930e
elis-wind <nbobkova174@gmail.com>
2023-11-11 18:33:42 +0100

    next day function

--- Data-Structures-and-Algorithms/01.Problem-solving/days_count.py
+++ Data-Structures-and-Algorithms/01.Problem-solving/days_count.py
@@ -7,3 +7,16 @@ def nextDay(year, month, day):
             return year, month + 1, 1
         else:
             return year + 1, 1, 1
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False 
+
+    
